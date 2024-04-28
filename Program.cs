a17f36d4f49853ab1bcdffe772d990ba9a76d556
Victoria <123573080+Vshagina@users.noreply.github.com>
2024-04-28 23:08:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+class Programm
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Car(string model)
+        {
+            Model = model;
+        }
+    }
+    public class Garage
+    {
+        private List<Car> cars;
+        public Garage()
+        {
+            cars = new List<Car>();
+        }
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+        public delegate void CarDelegate(Car car);
+        public void WashAllCars(CarDelegate carDelegate)
+        {
+            foreach (Car car in cars)
+            {
+                carDelegate(car);
+            }
+        }
+    }
+    public class Washer
+    {
+        public void Wash(Car car)
+        {
+            Console.WriteLine($"Мойка машины марки: {car.Model}");
+        }
+    }
+    public static void Main()
+    {
+        Car car1 = new Car("Audi");
+        Car car2 = new Car("Chery");
+        Car car3 = new Car("Toyota");
+
+        Garage garage = new Garage();
+        garage.AddCar(car1);
+        garage.AddCar(car2);
+        garage.AddCar(car3);
+
+        Washer washer = new Washer();
+
+        Garage.CarDelegate carDelegate = washer.Wash;
+        garage.WashAllCars(carDelegate);
+    }
+}
